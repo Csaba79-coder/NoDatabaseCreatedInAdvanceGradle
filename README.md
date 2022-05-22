0e84a1633c179659a5d99e2df20ad36f7f786579
Csaba79-coder <csabavadasz79@gmail.com>
2022-05-22 22:09:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Good to know how you can run your script under IntelliJ
+
+incl. all the setup!
---
+++ src/main/java/com/csaba79coder/nodatabasecreatedinadvancegradle/NoDatabaseCreatedInAdvanceGradleApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.nodatabasecreatedinadvancegradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoDatabaseCreatedInAdvanceGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoDatabaseCreatedInAdvanceGradleApplication.class, args);
+    }
+
+}
